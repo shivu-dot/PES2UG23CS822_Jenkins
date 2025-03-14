f90cf9d3a3a78f4a01116c80b8e71d7ea1f78415
shivu-dot <shivusp506@gmail.com>
2025-03-15 01:33:06 +0530

    Updated hello.cpp to test webhook trigger

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,11 @@
-//File: hello.cpp
+// File: hello.cpp
 #include <iostream>
 using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "I have successfully built and run this\n";
+  cout << "Webhook trigger is working!\n"; // Add this new line
   return 0;
 }
